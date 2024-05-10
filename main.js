81aca1881c62088686428661a5215babf47229bb
Salman-sajib <sfsajib@gmail.com>
2024-05-10 12:19:20 +0600

    factory function

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function () {
-    console.log('Draw');
-  },
-};
+// const circle = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1,
+//   },
+//   draw: function () {
+//     console.log('Draw');
+//   },
+// };
 
+// circle.draw();
+
+// Factory function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log('draw');
+    },
+  };
+}
+
+const circle = createCircle(1);
 circle.draw();
