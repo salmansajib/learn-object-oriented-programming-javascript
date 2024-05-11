abf9d2cb2ff4f89f4e7af9eb43d9ae724672c6aa
Salman-sajib <sfsajib@gmail.com>
2024-05-11 11:59:05 +0600

    primitive and reference type values

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 // Factory function
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function () {
-      console.log('draw');
-    },
-  };
-}
+// function createCircle(radius) {
+//   return {
+//     radius,
+//     draw: function () {
+//       console.log('draw');
+//     },
+//   };
+// }
 
-const circle = createCircle(1);
-circle.draw();
+// const circle = createCircle(1);
 
 // Condtructor function
-function Circle(radius) {
-  console.log('this', this);
-  this.radius = radius;
-  this.draw = function () {
-    console.log('draw');
-  };
+// function Circle(radius) {
+//   console.log('this', this);
+//   this.radius = radius;
+//   this.draw = function () {
+//     console.log('draw');
+//   };
+// }
+
+// console.log(Circle.name);
+
+// const another = new Circle(1);
+
+// Primitive type
+let number = 10;
+
+function increase(number) {
+  number++;
 }
-const another = new Circle(1);
+increase(number);
+console.log(number);
+
+// Reference type
+let obj = { value: 10 };
+
+function increment(obj) {
+  obj.value++;
+}
+increment(obj);
+console.log(obj);
